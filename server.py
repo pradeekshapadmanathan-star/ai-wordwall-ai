071d5ad081c4f8cd1f1bfe714ed42b9e7665503d
Pradeeksha <pradeekshapadmanathan@gmail.com>
2026-02-28 21:19:48 +0530

    fix render port

--- server.py
+++ server.py
@@ -185,5 +185,5 @@ Each section must contain exactly 5 items.
 # Run App
 # -----------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
